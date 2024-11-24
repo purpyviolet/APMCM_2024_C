2008d935852472228c46d69aff5461235325779b
purpyviolet <epiphyllum520@outlook.com>
2024-11-24 16:34:09 +0800

    stack chart pic updated

--- Q1_With_Stack_Chart.py
+++ Q1_With_Stack_Chart.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用SimHei字体
+plt.rcParams['font.sans-serif'] = ['Times New Roman']  # 使用times new roman字体
 plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
 # 1. 数据定义
 years = np.array([2019, 2020, 2021, 2022, 2023])  # 年份
